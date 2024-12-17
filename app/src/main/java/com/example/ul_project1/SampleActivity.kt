818cfbb8274c2c008dd4b3a47c0c551053babe8c
papoutsistavroula@gmail.com <papoutsistavroula@gmail.com>
2024-12-17 13:37:12 +0200

    Change Login and Register Activities to Fragments-Same functionality

--- app/src/main/java/com/example/ul_project1/SampleActivity.kt
+++ app/src/main/java/com/example/ul_project1/SampleActivity.kt
@@ -1,48 +1,50 @@
 package com.example.ul_project1
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 
-class SampleActivity
-    : AppCompatActivity(), FragmentA.EventListener {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class SampleActivity : AppCompatActivity() {
+
+    private lateinit var switchButton: Button
+    private lateinit var credentialsManager: CredentialsManager
+    private var isRegisterFragmentVisible = false //which fragment is active
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sample)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        findViewById<Button>(R.id.switch_button).setOnClickListener {
+        credentialsManager = CredentialsManager()
+        //we use the button to change the fragment
+        switchButton = findViewById(R.id.switch_button)
 
-            val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view)
-
-            supportFragmentManager.commit {
-                if (currentFragment is FragmentA) {
-                    replace<FragmentB>(R.id.fragment_container_view)
-                    addToBackStack(null)
-                } else {
-                    supportFragmentManager.popBackStack()
-                }
+        switchButton.setOnClickListener {
+            if (isRegisterFragmentVisible) {
+                loadFragment(LoginFragment()) //if RegisterFragment is active, load LoginFragment
+            } else {
+                loadFragment(RegisterFragment()) //if LoginFragment is active, load RegisterFragment
             }
         }
+        //at the beginning we load the LoginFragment
+        loadFragment(LoginFragment())
     }
 
-    override fun onGoToBRessed() {
-        Log.d("Preview", "onGoToBPressed in activity")
-        supportFragmentManager.commit{
-            replace<FragmentB>(R.id.fragment_container_view)
+
+    fun getCredentialsManager(): CredentialsManager {
+        return credentialsManager
+    }
+
+     fun loadFragment(fragment: Fragment) {
+        //supportFragmentManager to change fragments
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, fragment)
             addToBackStack(null)
         }
+        //update the flag in which fragment is active
+        isRegisterFragmentVisible = fragment is RegisterFragment
     }
 }
